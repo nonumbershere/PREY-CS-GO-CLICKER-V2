f9345bcf6b01b2b1bb06768057914b1e2277cb6c
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-09-24 13:15:36 -0400

    Update must_run.js

--- must_run.js
+++ must_run.js
@@ -1,4 +1,4 @@
 // This line prints "MUST-RUN ACTIVATED"
 console.log("MUST-RUN ACTIVATED");
 // Other Code
-alert("Thank you for using Prey Client! I will add many more stuff in the future.")
+console.log("By Lapide")
