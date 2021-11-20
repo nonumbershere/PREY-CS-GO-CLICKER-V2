5093c5480232a6789663e1c548afa5a57f034eac
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-11-20 18:34:44 -0500

    Update must_run.js

--- must_run.js
+++ must_run.js
@@ -2,3 +2,9 @@
 console.log("MUST-RUN ACTIVATED");
 // Other Code
 console.log("By Lapide")
+
+var discord = confirm("Would you like to join the official Discord Server?");
+
+if (discord) {
+open('https://discord.gg/cEPKrx96nk')
+}
