2e1f8786e95c0ccb781ff66856475a3b4e3d2662
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-11-20 18:35:43 -0500

    Update must_run.js

--- must_run.js
+++ must_run.js
@@ -7,4 +7,5 @@ var discord = confirm("Would you like to join the official Discord Server?");
 
 if (discord) {
 open('https://discord.gg/cEPKrx96nk')
+  alert("If you don't see the Discord server then try this: https://discord.gg/cEPKrx96nk");
 }
